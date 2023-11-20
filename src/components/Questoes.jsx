29cc8c0a50bb4b0f88b3aa99dea52f3202af8db5
gabrielaamorim <70036002+gabrielaamorim@users.noreply.github.com>
2023-11-20 18:34:14 -0300

    Primeiros arquivos

---
+++ src/components/Questoes.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { FaPencil, FaTrashCan } from 'react-icons/fa6';
+
+export default function QuestoesUsuarios() {
+    
+}
